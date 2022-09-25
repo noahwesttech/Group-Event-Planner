b43e5930e3edc4a2a324b6216c8df1c1766d87be
Mike Rugh <mike.rugh@gmail.com>
2022-09-25 16:51:31 -0700

    adds routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,4 +3,40 @@ const { Event, User, Item } = require('../models');
 const withAuth = require('../utils/auth');
 const Sequelize = require('sequelize');
 
+// need to add withAuth to these routes after testing
+
+// gets all events and renders homepage
+router.get('/', async (req, res) => {
+  try {
+    const eventData = await Event.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const events = eventData.map((event) => event.get({ plain: true }));
+
+    res.status(200).json(events);
+    // res.render('homepage', {
+    //   events,
+    //   logged_in: req.session.logged_in,
+    // });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// redirects to homepage if logged in
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
 module.exports = router;
