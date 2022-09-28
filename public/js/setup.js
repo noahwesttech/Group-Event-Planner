fd6ec27eec1310368bdaf25263c99a7068dd9491
Dina <dinamshawky@gmail.com>
2022-09-28 16:40:40 -0700

    description field

--- public/js/setup.js
+++ public/js/setup.js
@@ -1,1 +1,50 @@
-#!erased what i had written, was in wrong language!#
+const newEventFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("#event-input").value.trim();
+  const date = document.querySelector("#event-date").value.trim();
+  const location = document.querySelector("#event-location").value.trim();
+  const description = document.querySelector("#event-desc").value.trim();
+
+  const inviteEmailsAsString = document
+    .querySelector("#invite-email")
+    .value.trim();
+
+  let inviteEmailsAsArray = [];
+
+  // the split function would put an empty string at index 0
+  // if nothing was entered into the box by the user. This ensures
+  // we only split the string whenever the string has a value.
+  if (inviteEmailsAsString) {
+    inviteEmailsAsArray = inviteEmailsAsString.split(" ");
+  }
+
+  if (title > 0 && inviteEmailsAsArray.length > 0) {
+    const response = await fetch("/api/events", {
+      method: "post",
+      body: JSON.stringify({
+        event_title: title,
+        event_location: location,
+        event_date: date,
+        event_description: description,
+        invite_emails: inviteEmailsAsArray,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+    }
+  } else {
+    alert(
+      "Not enough information. Please fill out the entire form before submitting."
+    );
+    return;
+  }
+};
+
+document
+  .getElementById("event_new_form")
+  .addEventListener("submit", newEventFormHandler);
