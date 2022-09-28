7fa78701392cb7cb6677892c57de5cab98ac1d47
Mike Rugh <mike.rugh@gmail.com>
2022-09-28 15:35:21 -0700

    adds event location and description to event model

--- seeds/Event-seeds.js
+++ seeds/Event-seeds.js
@@ -3,6 +3,8 @@ const { Event } = require("../models");
 const eventdata = [
   {
     event_title: "Birthday Party",
+    event_location: "123 Main Street",
+    event_description: "Birthday Party",
     user_id: 1,
     invite_emails: [
       "test1@test.com", 
@@ -13,6 +15,8 @@ const eventdata = [
   },
   {
     event_title: "Baby Shower",
+    event_location: "123 Sesame Street",
+    event_description: "Baby Shower",
     user_id: 2,
     invite_emails: [
       "test10@test.com",
@@ -23,6 +27,8 @@ const eventdata = [
   },
   {
     event_title: "Retirement Party",
+    event_location: "123 Elm Street",
+    event_description: "Retirement Party",
     user_id: 3,
     invite_emails: [
       "test20@test.com",
@@ -33,6 +39,8 @@ const eventdata = [
   },
   {
     event_title: "Bachelor Party",
+    event_location: "123 Party Street",
+    event_description: "Bachelor Party",
     user_id: 4,
     invite_emails: [
       "test30@test.com",
@@ -43,6 +51,8 @@ const eventdata = [
   },
   {
     event_title: "Hike",
+    event_location: "123 Trail Street",
+    event_description: "Hike",
     user_id: 5,
     invite_emails: [
       "test40@test.com",
