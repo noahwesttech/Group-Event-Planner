e331d96d1c83331d81714b62087d15708d2a14cc
Dina <dinamshawky@gmail.com>
2022-09-28 10:54:38 -0700

    delete Item file

---
+++ public/js/delete-item.js
@@ -0,0 +1,27 @@
+async function deleteFormHandler(event) {
+  event.preventDefault();
+
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  const response = await fetch(`/api/event/${id}`, {
+    method: "DELETE",
+    body: JSON.stringify({
+      event_id: id,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/home");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document
+  .querySelector(".delete-btn")
+  .addEventListener("click", deleteFormHandler);
