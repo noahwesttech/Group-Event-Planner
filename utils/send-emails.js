790fcc393463ac5bb7b524f2e8870e8a2ccee50e
Mike Rugh <mike.rugh@gmail.com>
2022-09-28 21:41:36 -0700

    changes env config

--- utils/send-emails.js
+++ utils/send-emails.js
@@ -1,6 +1,8 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
+
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
