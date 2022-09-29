e0e9872986343a1b6de9ff6686f3d17de365c24d
Dina <dinamshawky@gmail.com>
2022-09-29 15:58:36 -0700

    icon styling

--- public/js/delete-event.js
+++ public/js/delete-event.js
@@ -1,21 +1,21 @@
 const delButtonHandler = async (event) => {
-    console.log('clicked');
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-    console.log('id:', id);
+  console.log("clicked");
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+    console.log("id:", id);
     const response = await fetch(`/api/event/${id}`, {
-      method: 'DELETE',
+      method: "DELETE",
     });
 
     if (response.ok) {
       document.location.replace(`/`);
     } else {
-      alert('Failed to delete event');
+      alert("Failed to delete event");
       console.log(response);
     }
   } else {
-    console.log('No data-id');
+    console.log("No data-id");
   }
 };
 
-document.querySelector('.d-btn').addEventListener('click', delButtonHandler);
+document.querySelector(".d-btn").addEventListener("click", delButtonHandler);
